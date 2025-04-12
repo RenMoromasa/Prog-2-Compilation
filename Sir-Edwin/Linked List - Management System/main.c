dd4571bbbd7ed54f54e4a7ebb788cb1e18a6645b
Tediisu <https://github.com/Tediisu>
2025-04-12 08:48:01 +0800

    Added Management System

--- Sir-Edwin/Linked List - Management System/main.c
+++ Sir-Edwin/Linked List - Management System/main.c
@@ -5,7 +5,57 @@
 
 int main() {
     // complete the main function
-    
+    Student *head = NULL;
+    int choice, studID;
+    char name[32];
+    float gpa;
+        
+    do 
+    {
+        menu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar();
+        
+        switch (choice)
+        {
+            case 1:
+                printf("Enter student name: ");
+                fgets(name, 32, stdin);
+                strtok(name, "\n");
+                
+                printf("Enter student ID: ");
+                scanf("%d", &studID);
+                getchar();
+                
+                printf("Enter student GPA: ");
+                scanf("%f", &gpa);
+                getchar();
+                
+                head = insertStudent(head, name, studID, gpa);
+                break;
+            case 2:
+                printf("Enter student ID to remove: ");
+                scanf("%d", &studID);
+                
+                head = deleteStudent(head, studID);
+                break;
+            case 3:
+                displayStudent(head);
+                break;
+            case 4:
+                printf("Enter student name to search: ");
+                fgets(name, 32, stdin);
+                strtok(name, "\n");
+                
+                searchStudentByName(head, name);
+                break;
+            case 5:
+                printf("Exiting program.\n");
+                break;
+        }
+        
+    } while (choice != 5);
 
     return 0;
 }
