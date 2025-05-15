e81658cd1e3ddf30d3418e89df6b1dc9193c2cfe
RenMoromasa <ren.moromasa90@gmail.com>
2025-05-16 00:06:27 +0800

    Added v3 and v4

---
+++ Array List/ArrayListV4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ARRAY LIST V4
+// Structure Definition
+#define MAX 100
+typedef struct {
+    char *elems;
+    int count;
+} *List;
+
+// Initialize List
+void initList (List *L){
+    
+}
+
+// Search
+void search (List *L, char val){
+
+}
+
+// Insert First
+void insertFront(List *L, char val){
+
+}
+
+// Insert Last
+void insertLast(List *L, char val){
+
+}
+
+// Insert At Position
+void insertAt (List* L, char val){
+
+}
+
+// Insert Sorted
+void insertSorted (List *L, char val){
+
+}
+
+// Delete Front
+void deleteFront (List *L){
+
+}
+// Delete Last
+void deleteLast (List *L){
+
+}
+// Delete At Position
+void deleteAt (List *L, int pos){
+    
+}
